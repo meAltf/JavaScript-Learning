246c73676344e832fed682f27610dc154716d304
meAltf <alataf.dce.cs1@gmail.com>
2023-10-27 18:52:54 +0530

    stack and Heap memory

---
+++ 01_basics/09_stack_and_Heap.js
@@ -0,0 +1,21 @@
+// Stack(Primitive)
+
+// Heap(Non-Primitive)
+
+let myYoutubename = "alatafdotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "alataf@google.com",
+    upi: "user@idbi"
+}
+
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
